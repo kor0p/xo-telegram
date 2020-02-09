a78a2dbbef65266a8e4dc79a248ad9ea002b6680
kor0p <viktorua25.03@gmail.com>
2020-02-09 20:13:01 +0200

    local config

--- bot/config.py
+++ bot/config.py
@@ -1,2 +1,6 @@
-token = '<your token here>'
-path_to_db = '/database.sqlite'
+# token = '662584029:AAH77_sOpwyEw6CF2-D88IbG-571jyMrsaE' # xoucubot
+# token = '699140868:AAHdJGrcN9X2BvKKoezRFrqGCw_zgB9lK6g' # m0xbot
+token = '675808981:AAG8pwkvZeAB4Y9fyyR2-TgaEK-dQTy9BL8'  # justnewtestbot
+path_to_db = 'postgres://lpihuqtbzvtjei:7d5aae2fc13cf20db8bd57ed085fdd173907668df113167c7350c6b09015c435@ec2-46-137-177-160.eu-west-1.compute.amazonaws.com:5432/d109frgjeam57q'
+# ip_address = '157.230.17.50'
+# port = 443
