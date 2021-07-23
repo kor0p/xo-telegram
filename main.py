e7c0295f9770651fa3d2a9eb3b45f6183aafe2b3
kor0p <viktorua25.03@gmail.com>
2021-07-23 22:50:03 +0300

    empty commit to trigger redeploy

--- main.py
+++ main.py
@@ -7,4 +7,4 @@ from telebot import types
 def handle_telegram(request):
     body = request.get_json()
     bot.process_new_updates([types.Update.de_json(body)])
-    return '{"success":true}'
+    return '{"success": true}'
