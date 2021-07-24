eca34e62daceae9eecc676d13d8d7c1fff465611
kor0p <viktorua25.03@gmail.com>
2021-07-24 16:07:46 +0300

    return to native http handler

--- main.py
+++ main.py
@@ -1,72 +1,58 @@
 import os
 
-# import json
-# from time import sleep
-# from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+from time import sleep
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import bot.handlers.__main__
 from bot.bot import bot
 
 from telebot import types
 
-from flask import Flask, request
 
-app = Flask(__name__)
+class handler(BaseHTTPRequestHandler):
+    server_version = 'WebhookHandler/1.0'
 
+    def do_HEAD(self):
+        self.send_response(200)
+        self.end_headers()
 
-# SERVER SIDE
-@app.route('/' + bot.token, methods=['POST'])
-def get_message():
-    bot.process_new_updates([types.Update.de_json(request.stream.read().decode('utf-8'))])
-    return '!', 200
+    def do_GET(self):
+        bot.remove_webhook()
+        bot.set_webhook(url='https://xo-telegram-bot.herokuapp.com/' + bot.token)
 
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Webhook set')
 
-@app.route('/')
-def webhook():
-    bot.remove_webhook()
-    bot.set_webhook(url='https://xo-telegram-bot.herokuapp.com/' + bot.token)
-    return '!', 200
+    def do_DELETE(self):
+        bot.remove_webhook()
+
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b'Webhook deleted')
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+        post_data = self.rfile.read(content_length)  # ###### # <--- Gets the data itself
+        body = json.loads(post_data.decode())
+
+        bot.process_new_updates([types.Update.de_json(body)])
+
+        self.send_response(204)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(b'')
+        return
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
-
-
-# class handler(BaseHTTPRequestHandler):
-#     server_version = 'WebhookHandler/1.0'
-#
-#     def do_HEAD(self):
-#         self.send_response(200)
-#         self.end_headers()
-#
-#     def do_GET(self):
-#         self.send_response(200)
-#         self.end_headers()
-#
-#     def do_POST(self):
-#         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-#         post_data = self.rfile.read(content_length)  # ###### # <--- Gets the data itself
-#         body = json.loads(post_data.decode())
-#
-#         bot.process_new_updates([types.Update.de_json(body)])
-#
-#         self.send_response(204)
-#         self.send_header('Content-type', 'text/plain')
-#         self.end_headers()
-#         self.wfile.write(b'')
-#         return
-#
-#
-# PORT = int(os.environ.get('PORT'))
-# if not PORT or PORT not in (80, 88, 443, 8443):
-#     PORT = 8443
-#
-# httpd = HTTPServer(('', PORT), handler)
-# print('Starting httpd...\n')
-# sleep(1)
-# try:
-#     httpd.serve_forever()
-# except KeyboardInterrupt:
-#     pass
-# httpd.server_close()
-# print('Stopping httpd...\n')
+    httpd = HTTPServer(('', int(os.environ.get('PORT', 5000))), handler)
+    print('Starting httpd...\n')
+    sleep(1)
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print('Stopping httpd...\n')
