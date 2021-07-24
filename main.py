32f27538005a120a44176667e6f942246e008aa5
kor0p <viktorua25.03@gmail.com>
2021-07-24 15:43:54 +0300

    try to use flask

--- main.py
+++ main.py
@@ -1,39 +1,72 @@
 import os
-import json
-from time import sleep
-from http.server import BaseHTTPRequestHandler, HTTPServer
+
+# import json
+# from time import sleep
+# from http.server import BaseHTTPRequestHandler, HTTPServer
 
 import bot.handlers.__main__
 from bot.bot import bot
 
 from telebot import types
 
+from flask import Flask, request
 
-class handler(BaseHTTPRequestHandler):
-    def do_POST(self):
-        content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
-        post_data = self.rfile.read(content_length)  # ###### # <--- Gets the data itself
-        body = json.loads(post_data.decode())
-
-        bot.process_new_updates([types.Update.de_json(body)])
-
-        self.send_response(204)
-        self.send_header('Content-type', 'text/plain')
-        self.end_headers()
-        self.wfile.write(b'')
-        return
+app = Flask(__name__)
 
 
-PORT = int(os.environ.get('PORT'))
-if not PORT or PORT not in (80, 88, 443, 8443):
-    PORT = 8443
+# SERVER SIDE
+@app.route('/' + bot.token, methods=['POST'])
+def get_message():
+    bot.process_new_updates([types.Update.de_json(request.stream.read().decode('utf-8'))])
+    return '!', 200
 
-httpd = HTTPServer(('', PORT), handler)
-print('Starting httpd...\n')
-sleep(1)
-try:
-    httpd.serve_forever()
-except KeyboardInterrupt:
-    pass
-httpd.server_close()
-print('Stopping httpd...\n')
+
+@app.route('/')
+def webhook():
+    bot.remove_webhook()
+    bot.set_webhook(url='https://xo-telegram-bot.herokuapp.com/' + bot.token)
+    return '!', 200
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+
+
+# class handler(BaseHTTPRequestHandler):
+#     server_version = 'WebhookHandler/1.0'
+#
+#     def do_HEAD(self):
+#         self.send_response(200)
+#         self.end_headers()
+#
+#     def do_GET(self):
+#         self.send_response(200)
+#         self.end_headers()
+#
+#     def do_POST(self):
+#         content_length = int(self.headers['Content-Length'])  # <--- Gets the size of data
+#         post_data = self.rfile.read(content_length)  # ###### # <--- Gets the data itself
+#         body = json.loads(post_data.decode())
+#
+#         bot.process_new_updates([types.Update.de_json(body)])
+#
+#         self.send_response(204)
+#         self.send_header('Content-type', 'text/plain')
+#         self.end_headers()
+#         self.wfile.write(b'')
+#         return
+#
+#
+# PORT = int(os.environ.get('PORT'))
+# if not PORT or PORT not in (80, 88, 443, 8443):
+#     PORT = 8443
+#
+# httpd = HTTPServer(('', PORT), handler)
+# print('Starting httpd...\n')
+# sleep(1)
+# try:
+#     httpd.serve_forever()
+# except KeyboardInterrupt:
+#     pass
+# httpd.server_close()
+# print('Stopping httpd...\n')
